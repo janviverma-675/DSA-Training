cc2bcfab435508ed48fa395d5b40f5305f06659c
janvi verma <janviverma675@gmail.com>
2026-03-27 13:02:51 +0530

    Time: 2 ms (95.53%) | Memory: 45.2 MB (98.63%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public static void Solve(int i , int[] arr , int target , List<List<Integer>> ans , List<Integer> curr)
+    {
+        if(target == 0)
+        {
+            ans.add(new ArrayList<>(curr)) ;
+            return ;
+        }
+        if(i == arr.length)
+        {
+            
+            return ;
+        }
+        if(target >= arr[i])
+        {
+            curr.add(arr[i]) ;
+            Solve(i,arr,target-arr[i],ans,curr) ;
+            curr.remove(curr.size()-1) ;
+        }
+      
+
+        Solve(i+1,arr,target,ans,curr) ;
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<List<Integer>> ans = new ArrayList() ;
+        Solve(0,candidates,target,ans,new ArrayList<Integer>()) ;
+        return ans ;
+    }
+}
